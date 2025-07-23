344c37b930b25944d215286e9c9d7a2547a7707c
petarkosic <avt3914@gmail.com>
2025-07-23 23:59:16 +0200

    add: initial setup for alert manager

---
+++ backend/alert-manager/src/main/java/com/alert_manager/AlertManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alert_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertManagerApplication.class, args);
+	}
+
+}
---
+++ backend/alert-manager/src/test/java/com/alert_manager/AlertManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alert_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
